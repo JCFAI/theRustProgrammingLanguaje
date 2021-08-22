a0d18de56ea330e455293b5553af98ce8d727a53
JCF <jorgecarlosfossati@gmail.com>
2021-08-22 00:28:10 -0300

    first commit Pag 43 - 49

---
+++ chap_1/Pag_43/hello_cargo/target/rls/debug/deps/hello_cargo-9a4c38dbbe5c4c8c.d
@@ -0,0 +1,5 @@
+e:\RUST\theRustProgrammingLanguaje\theRustProgrammingLanguaje\chap_1\Pag_43\hello_cargo\target\rls\debug\deps\hello_cargo-9a4c38dbbe5c4c8c.rmeta: src\main.rs
+
+e:\RUST\theRustProgrammingLanguaje\theRustProgrammingLanguaje\chap_1\Pag_43\hello_cargo\target\rls\debug\deps\hello_cargo-9a4c38dbbe5c4c8c.d: src\main.rs
+
+src\main.rs:
